220bcde6802b7824a7d3bb5296ebfb0ae740b391
Morenojuand08 <Morenojuand08@gmail.com>
2023-11-01 12:34:45 -0500

    Notificaciones Reactivas

--- primerProyecto/src/App.vue
+++ primerProyecto/src/App.vue
@@ -1,47 +1,95 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue';
+  let notification= ref (0)
+
+function addValue(){
+  notification.value++
+  console.log(notification)
+}
+
+function minusValue(){
+  if(notification.value>=1){
+    notification.value--
+  }
+}
+
+addValue()
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<div class="wrapper">
+  <div class="profile">
+    <div class="notification" v-show="notification > 0">{{ notification }}</div>
+  </div>
+  <div class="box">
+    <div class="button" @click="minusValue">-</div>
+    <div class="button" @click="addValue">+</div>
+  </div>
+  <div v-if="notification == 0">Buuuu! Nadie te escribe😢</div>
+  <div v-else>Revisa tu inbox</div>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .wrapper {
+    background-color: #349998;
+    width: 60%;
+    border-radius: 8px;
+    margin: 0 auto;
+    padding-top: 32px;
+    overflow: hidden;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .profile {
+    background-color: purple;
+    width: 120px;
+    height: 120px;
+    margin: 0 auto;
+    border-radius: 100%;
+    position: relative;
+    background-image: url('https://elireview.com/wp-content/uploads/2016/12/reed-profile-square.jpg');
+    background-size: cover;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.notification{
+  position: absolute;
+  background-color: #f24006;
+  min-width: 18px;
+  height: 18px;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  top: 0;
+  right: 0;
 }
+
+  .box {
+    text-align: center;
+    padding-bottom: 16px;
+    margin-top: 12px;
+  }
+
+  .button {
+  background-color: peru;
+  border-radius: 4px;
+  padding: 2px 0;
+  font-weight: bold;
+  color: white;
+  display: inline-block;
+  width: 40px;
+  cursor: pointer;
+  
+  }
+
+  .button:first-child {
+    margin-right: 8px;
+  }
+
+  .button:last-child{
+    margin-left: 8px;
+  }
+
 </style>
